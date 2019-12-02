9956fadc91c4bf4f0bbba46a34d6b535edf418c5
Martha Sharpe <marthasharpe2020@gmail.com>
2019-12-02 14:13:13 -0500

    Add Navbar

--- src/app/App.js
+++ src/app/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Home from './../home/Home.js';
 import About from './../about/About.js';
+import Navigate from './../navbar/Navbar.js';
 
 const App = () => {
   return (
     <div className="App">
       <Home />
+      <Navigate />
       <About />
     </div>
   );
---
+++ src/navbar/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Navbar.css';
+import { Navbar, Nav } from 'react-bootstrap';
+
+const Navigate = () => {
+    return (
+<Navbar collapseOnSelect sticky="top" expand="lg" bg="dark" variant="dark">
+  <Navbar.Brand href="#home">MarthaSharpeCodes</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="justify-content-end mr-auto">
+      <Nav.Link href="#about">About</Nav.Link>
+      <Nav.Link href="#projects">Projects</Nav.Link>
+    </Nav>
+    <Nav>
+      <Nav.Link href="#linkedin">LinkedIn</Nav.Link>
+      <Nav.Link href="#github">GitHub</Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+    )
+}
+
+export default Navigate;
