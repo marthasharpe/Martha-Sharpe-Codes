af5c2fd737821fb66eab5e6e2fd281fa0dd26088
Martha Sharpe <marthasharpe2020@gmail.com>
2019-12-03 17:49:20 -0500

    build folder

---
+++ build/precache-manifest.6d97df904a7819cf3a6f9067aa188fdd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4344936c9127b71f74ea93434de345f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca177a85f5c2bbb692e2",
+    "url": "/static/css/2.81d7566b.chunk.css"
+  },
+  {
+    "revision": "9551d9dc59db26a81579",
+    "url": "/static/css/main.0a8cd515.chunk.css"
+  },
+  {
+    "revision": "ca177a85f5c2bbb692e2",
+    "url": "/static/js/2.f59425c5.chunk.js"
+  },
+  {
+    "revision": "9551d9dc59db26a81579",
+    "url": "/static/js/main.586cc12d.chunk.js"
+  },
+  {
+    "revision": "0027df7872c29bbbef2e",
+    "url": "/static/js/runtime-main.29ed2af3.js"
+  },
+  {
+    "revision": "5c39408f30d4d4116eeddd2b24d2a5f8",
+    "url": "/static/media/Martha Sharpe Resume.5c39408f.pdf"
+  },
+  {
+    "revision": "b4b9ce6b52e32cd88c488395de3cee6c",
+    "url": "/static/media/profile-pic-crop copy.b4b9ce6b.jpg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  }
+]);
