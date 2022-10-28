9ab5c0a4425de75be2f4f533c56c9ce2f0d86fb4
Martha Sharpe <marthasharpe2020@gmail.com>
2022-10-28 08:27:43 -0500

    update files

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,34 +1,45 @@
-import React from 'react';
-import { Image, Jumbotron, Nav } from 'react-bootstrap';
-import FontAwesome from 'react-fontawesome';
-import pic from '../profile-pic-crop.jpg';
-import './Home.css';
-
+import React from "react";
+import { Image, Jumbotron, Nav } from "react-bootstrap";
+import FontAwesome from "react-fontawesome";
+import pic from "../assets/new-profile-pic.jpg";
+import "./Home.css";
 
 const Home = () => {
-    return (
-        <Jumbotron id="home" fluid>
-            <div className="title-container">
-                <Image src={pic} className="profile-pic" alt="profile" roundedCircle fluid/>
-                <h1>Martha Sharpe</h1>
-                <h3>Software Engineer</h3>
-                <Nav>
-                  <Nav.Link href="https://www.linkedin.com/in/marthasharpe2020/" target="_blank">
-                    <FontAwesome name="linkedin" size="2x" className="social-link"/>
-                  </Nav.Link>
-                  <Nav.Link href="https://twitter.com/SharpeMartha" target="_blank">
-                    <FontAwesome name="twitter" size="2x" className="social-link"/>
-                  </Nav.Link>
-                  <Nav.Link href="https://github.com/marthasharpe" target="_blank">
-                    <FontAwesome name="github" size="2x" className="social-link"/>
-                  </Nav.Link>
-                  <Nav.Link href="https://www.youtube.com/channel/UCqL_YXyK5g7rr4MmhqVCAGA" target="_blank">
-                    <FontAwesome name="youtube" size="2x" className="social-link"/>
-                  </Nav.Link>
-                </Nav>
-            </div>
-        </Jumbotron>
-    )
-}
+  return (
+    <Jumbotron id="home" fluid>
+      <div className="title-container">
+        <Image
+          src={pic}
+          className="profile-pic"
+          alt="profile"
+          roundedCircle
+          fluid
+        />
+        <h1>Martha Sharpe</h1>
+        <h3>Software Engineer</h3>
+        <Nav>
+          <Nav.Link
+            href="https://www.linkedin.com/in/marthasharpe2020/"
+            target="_blank"
+          >
+            <FontAwesome name="linkedin" size="2x" className="social-link" />
+          </Nav.Link>
+          <Nav.Link href="https://twitter.com/SharpeMartha" target="_blank">
+            <FontAwesome name="twitter" size="2x" className="social-link" />
+          </Nav.Link>
+          <Nav.Link href="https://github.com/marthasharpe" target="_blank">
+            <FontAwesome name="github" size="2x" className="social-link" />
+          </Nav.Link>
+          <Nav.Link
+            href="https://www.youtube.com/channel/UCqL_YXyK5g7rr4MmhqVCAGA"
+            target="_blank"
+          >
+            <FontAwesome name="youtube" size="2x" className="social-link" />
+          </Nav.Link>
+        </Nav>
+      </div>
+    </Jumbotron>
+  );
+};
 
 export default Home;
--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -1,31 +1,36 @@
-import React from 'react';
-import { Nav, Navbar } from 'react-bootstrap';
-import logo from '../logo.png';
-import resume from './martha-sharpe-resume.pdf';
-import './Navbar.css';
+import React from "react";
+import { Nav, Navbar } from "react-bootstrap";
+import logo from "../assets/logo.png";
+import resume from "../assets/MarthaSharpeResume.pdf";
+import "./Navbar.css";
 
 const Navigate = () => {
-    return (
-      <Navbar collapseOnSelect sticky="top" expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand href="#App">
-          <img
-            alt=""
-            src={logo}
-            width="40"
-            height="40"
-            className="d-inline-block align-top"
-          />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav"  className="justify-content-end">
-          <Nav>
-            <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#projects">Projects</Nav.Link>
-            <Nav.Link href={resume} target="_blank">Resumé</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    )
-}
+  return (
+    <Navbar collapseOnSelect sticky="top" expand="lg" bg="dark" variant="dark">
+      <Navbar.Brand href="#App">
+        <img
+          alt=""
+          src={logo}
+          width="40"
+          height="40"
+          className="d-inline-block align-top"
+        />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse
+        id="responsive-navbar-nav"
+        className="justify-content-end"
+      >
+        <Nav>
+          <Nav.Link href="#about">About</Nav.Link>
+          <Nav.Link href="#projects">Projects</Nav.Link>
+          <Nav.Link href={resume} target="_blank">
+            Resumé
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
 
 export default Navigate;
